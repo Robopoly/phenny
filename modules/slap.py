48ce9286b64bfead8d10e517e957959737d034ad
Andrew Watson <andy@watsons.ch>
2011-01-23 17:47:39 +0100

    slap: fetch from jenni

--- modules/slap.py
+++ modules/slap.py
@@ -7,19 +7,19 @@ Jenni (About): http://inamidst.com/phenny/
 
 import random
 
-def slap(jenni, input):
+def slap(phenny, input):
     """.slap <target> - Slaps <target>"""
     text = input.group().split()
     if len(text) < 2 or text[1].startswith('#'): return
-    if text[1] == jenni.nick:
-        if (input.nick not in jenni.config.admins):
+    if text[1] == phenny.nick:
+        if (input.nick not in phenny.config.admins):
             text[1] = input.nick
         else: text[1] = 'herself'
-    if text[1] in jenni.config.admins:
-        if (input.nick not in jenni.config.admins):
+    if text[1] in phenny.config.admins:
+        if (input.nick not in phenny.config.admins):
             text[1] = input.nick
     verb = random.choice(('slaps', 'kicks', 'destroys', 'annihilates', 'punches', 'teabags', 'roundhouse kicks', 'rusty hooks', 'pwns', 'owns'))
-    jenni.write(['PRIVMSG', input.sender, ' :\x01ACTION', verb, text[1], '\x01'])
+    phenny.write(['PRIVMSG', input.sender, ' :\x01ACTION', verb, text[1], '\x01'])
 slap.commands = ['slap', 'slaps']
 slap.priority = 'medium'
 
