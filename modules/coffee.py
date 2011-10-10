b2b1bf29f89d839cb17ad0ca8435355a4ed75235
Andrew Watson <andy@watsons.ch>
2011-10-10 15:56:15 +0300

    fix dumb spelling mistake in coffee.py

--- modules/coffee.py
+++ modules/coffee.py
@@ -1,5 +1,5 @@
 def coffee(phenny, input):
-    phenny.say("""Srgh orry, I'll start preparing coffee once you let me control \
+    phenny.say("""Sorry, I'll start preparing coffee once you let me control \
 the machine. No sooner."""
     )
 coffee.commands = ['coffee']
